d5d5177e59a7a9566420082557de12b937c5542c
ninhja <ariannaninh@gmail.com>
2025-08-18 20:10:18 -0700

    fixexx version 4 server/api scaffolding

--- version-4/server/src/index.js
+++ version-4/server/src/index.js
@@ -1,7 +1,8 @@
-/* Server/API for Countries Project Version 4
+/* --------------------------------
+Server/API for Countries App Version 4
 
-DB Fiddle with SQL Database Schema: 
-*/
+DB Fiddle Link: ______________
+----------------------------------*/
 
 /*----------------------------------
 Boilerplate Code to Set Up Server
@@ -14,15 +15,3 @@ Helper Functions
 /*----------------------------------
 API Endpoints
 ----------------------------------*/
-
-// GET /get-all-users
-
-// GET /get-newest-user
-
-// POST /add-one-user
-
-// GET /get-all-saved-countries
-
-// GET /save-one-country/:cca3
-
-// GET /update-one-country-count/:cca3
