27fde43f4a10b76571fc60e09794f247524ae89f
ruchanimbalkar <rucha.nimbalkar90@gmail.com>
2025-09-09 13:27:00 -0700

    added left arrow icon to the back link in country detail page

--- version-1/src/components/Card.jsx
+++ version-1/src/components/Card.jsx
@@ -4,6 +4,7 @@ export default function Card({country}){
             <img src={country.flags.png} alt={country.flags.alt}/>
             <div className="country-card-text">
                 <h2> {country.name.common} </h2>
+                
                 <p> <b> Population : </b>  {country.population} </p>
                 <p> <b> Region : </b> {country.region}  </p>
                 <p> {countryCapital} </p>
--- version-1/src/pages/CountryDetail.jsx
+++ version-1/src/pages/CountryDetail.jsx
@@ -5,6 +5,7 @@ export default function CountryDetail({countries}){
     //get this country's name from the URL parameter
     const countryName = useParams().countryName;
     console.log("Country Detail");
+    let leftArrowIcon = String.fromCodePoint(0x2190);
     let countryObject = {};
     for (let country of countries) {
         console.log(typeof country);
@@ -15,7 +16,7 @@ export default function CountryDetail({countries}){
     }
     //testing: console.log("countryObject : ", countryObject);
     return (<> 
-        <a href="/" className="country-detail-back-link" > Back </a>
+        <a href="/" className="country-detail-back-link" > {leftArrowIcon} Back </a>
         <div className="country-detail">
             <Card country={countryObject}/>
         </div>
