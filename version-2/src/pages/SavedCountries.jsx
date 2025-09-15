85b9f42f726906adf4000fc94c0737869daf5537
ruchanimbalkar <rucha.nimbalkar90@gmail.com>
2025-09-15 14:06:56 -0700

    form data stored in local storage

--- version-2/src/pages/SavedCountries.jsx
+++ version-2/src/pages/SavedCountries.jsx
@@ -1,12 +1,58 @@
 //import Form component
 import Form from "../components/Form";
+import { useState, useEffect } from "react";
 export default function SavedCountries({ countriesData }) {
-  console.log("Saved Countries");
+  //console.log("Saved Countries");
+  const [userFormInfo, setUserFormInfo] = useState(null);
+  const [formData, setFormData] = useState({
+    fullName: "",
+    email: "",
+    country: "",
+    bio: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    const checked = e.target.checked;
+    console.log(name, value, checked);
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+  };
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log("formData", formData);
+    setFormData({
+      fullName: "",
+      email: "",
+      country: "",
+      bio: "",
+    });
+    // Use JSON.stringify() to convert the object into a string before storing it.
+    let userFormData = JSON.stringify(formData);
+    //Save user data in local storage
+    localStorage.setItem("userInfo", userFormData);
+  }
+
+  useEffect(() => {
+    // Use localStorage.getItem() and JSON.parse() to get the original object back
+    setUserFormInfo(JSON.parse(localStorage.getItem("userInfo")));
+    console.log("userFormInfo : ", userFormInfo);
+  }, []);
+
   return (
     <>
       <main>
-        <h1> My Saved Countries</h1>
-        <Form />
+        <h1>
+          {userFormInfo === null
+            ? "My Saved Countries "
+            : `Hi ${userFormInfo.fullName}`}
+        </h1>
+        <div className="saved-countries-card"> </div>
+        <Form
+          onSubmit={handleSubmit}
+          onChange={handleChange}
+          formData={formData}
+        />
       </main>
     </>
   );
