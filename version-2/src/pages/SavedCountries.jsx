23a67ba9208211e1f9584ba3c1285c432eba31b3
ruchanimbalkar <rucha.nimbalkar90@gmail.com>
2025-09-16 11:20:49 -0700

    changed code for localstorage in SavedCountries

--- version-2/src/pages/SavedCountries.jsx
+++ version-2/src/pages/SavedCountries.jsx
@@ -3,13 +3,9 @@ import Form from "../components/Form";
 import { useState, useEffect } from "react";
 export default function SavedCountries({ countriesData }) {
   //console.log("Saved Countries");
+  const emptyFormState = { fullName: "", email: "", country: "", bio: "" };
   const [userFormInfo, setUserFormInfo] = useState(null);
-  const [formData, setFormData] = useState({
-    fullName: "",
-    email: "",
-    country: "",
-    bio: "",
-  });
+  const [formData, setFormData] = useState(emptyFormState);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -20,13 +16,10 @@ export default function SavedCountries({ countriesData }) {
 
   function handleSubmit(event) {
     event.preventDefault();
+    // update the user's info in state
+    setUserFormInfo(formData);
     console.log("formData", formData);
-    setFormData({
-      fullName: "",
-      email: "",
-      country: "",
-      bio: "",
-    });
+    setFormData(emptyFormState);
     // Use JSON.stringify() to convert the object into a string before storing it.
     let userFormData = JSON.stringify(formData);
     //Save user data in local storage
@@ -34,9 +27,14 @@ export default function SavedCountries({ countriesData }) {
   }
 
   useEffect(() => {
-    // Use localStorage.getItem() and JSON.parse() to get the original object back
-    setUserFormInfo(JSON.parse(localStorage.getItem("userInfo")));
-    console.log("userFormInfo : ", userFormInfo);
+    if (localStorage.getItem("userInfo")) {
+      //if condition is true
+      // get the original form object back.
+      // Use localStorage.getItem() and JSON.parse() to get the original object back
+      let profileDestringified = JSON.parse(localStorage.getItem("userInfo"));
+      setUserFormInfo(profileDestringified);
+      console.log("userFormInfo : ", userFormInfo);
+    }
   }, []);
 
   return (
