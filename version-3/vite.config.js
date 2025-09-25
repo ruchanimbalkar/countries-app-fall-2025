7078207903f34d4365650f91cb3b94af55f1301c
ruchanimbalkar <rucha.nimbalkar90@gmail.com>
2025-09-25 10:31:46 -0700

    updated two files

--- version-3/vite.config.js
+++ version-3/vite.config.js
@@ -1,11 +1,13 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vite.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "API URL GOES HERE",
+        target: "https://backend-answer-keys.onrender.com/",
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ""),
